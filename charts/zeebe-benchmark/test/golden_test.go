dcf2b7e82f80a61187f821a21c3b47f9379b8c57
Christopher Zell <zelldon91@googlemail.com>
2024-09-02 12:20:33 +0200

    test: add test for workers

--- charts/zeebe-benchmark/test/golden_test.go
+++ charts/zeebe-benchmark/test/golden_test.go
@@ -11,11 +11,11 @@ import (
 	"github.com/stretchr/testify/suite"
 )
 
-func TestGoldenCuratorDefaults(t *testing.T) {
+func TestGoldenDefaults(t *testing.T) {
 
 	chartPath, err := filepath.Abs("../")
 	require.NoError(t, err)
-	templateNames := []string{"clients-service", "leader-balancing-cron", "publisher", "starter", "timer", "worker", "benchmark-config"}
+	templateNames := []string{"clients-service", "leader-balancing-cron", "publisher", "starter", "timer", "worker", "benchmark-config", "workers"}
 
 	for _, name := range templateNames {
 		suite.Run(t, &golden.TemplateGoldenTest{
@@ -25,6 +25,30 @@ func TestGoldenCuratorDefaults(t *testing.T) {
 			GoldenFileName: name,
 			Templates:      []string{"templates/" + name + ".yaml"},
 			SetValues:      map[string]string{},
+		})
+	}
+}
+
+func TestGoldenWorkers(t *testing.T) {
+
+	chartPath, err := filepath.Abs("../")
+	require.NoError(t, err)
+	templateNames := []string{"workers"}
+
+	values := map[string]string{
+		"workers.otherWorker.jobType":  "otherWorker-job",
+		"workers.otherWorker.replicas": "1",
+		"workers.otherWorker.capacity": "10",
+	}
+
+	for _, name := range templateNames {
+		suite.Run(t, &golden.TemplateGoldenTest{
+			ChartPath:      chartPath,
+			Release:        "benchmark-test",
+			Namespace:      "benchmark-" + strings.ToLower(random.UniqueId()),
+			GoldenFileName: name,
+			Templates:      []string{"templates/" + name + ".yaml"},
+			SetValues:      values,
 		})
 	}
 }
