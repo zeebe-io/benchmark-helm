f454d88eabc40f7fc2ca044df1bfa3e68608ce42
Christopher Zell <zelldon91@googlemail.com>
2024-09-20 09:11:15 +0200

    test: add golden test for credentials

---
+++ charts/zeebe-benchmark/test/saas_configuration_test.go
@@ -0,0 +1,39 @@
+package test
+
+import (
+	"benchmark-helm/charts/zeebe-benchmark/test/golden"
+	"path/filepath"
+	"strings"
+	"testing"
+
+	"github.com/gruntwork-io/terratest/modules/random"
+	"github.com/stretchr/testify/require"
+	"github.com/stretchr/testify/suite"
+)
+
+func TestGoldenCredentials(t *testing.T) {
+	// Test which allows to verify also parent chart templates
+	// This makes sure that properties are correctly set
+	// OR configurations have been changed
+
+	chartPath, err := filepath.Abs("../")
+	require.NoError(t, err)
+	templateNames := []string{"credentials"}
+
+	for _, name := range templateNames {
+		suite.Run(t, &golden.TemplateGoldenTest{
+			ChartPath:      chartPath,
+			Release:        "benchmark-test",
+			Namespace:      "benchmark-" + strings.ToLower(random.UniqueId()),
+			GoldenFileName: "golden-credentials-" + name,
+			Templates:      []string{"templates/" + name + ".yaml"},
+			SetValues: map[string]string{
+				"saas.enabled":                  "true",
+				"saas.credentials.clientId":     "clientId",
+				"saas.credentials.clientSecret": "clientSecret",
+				"saas.credentials.authServer":   "authServer",
+				"saas.credentials.zeebeAddress": "zeebeAddress",
+			},
+		})
+	}
+}
